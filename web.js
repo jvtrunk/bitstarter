763fd290dfde356ca3b918e812fd3f6482da749c
Joseph Trunk <jvtrunk@gmail.com>
2013-07-11 00:42:12 +0000

    Reading and outputting index.html

--- web.js
+++ web.js
@@ -4,6 +4,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+/*
   fs.readFileSync('./index.html', function (err, data) {
     if (err) throw err;
     var my_str = data.toString();
