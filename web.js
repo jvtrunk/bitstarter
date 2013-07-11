0df6c1afec8e70dc5a7a58fabcf3f4af0075b7dc
Joseph Trunk <jvtrunk@gmail.com>
2013-07-11 00:26:14 +0000

    Reading and outputting index.html

--- web.js
+++ web.js
@@ -3,10 +3,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  fs.readFileSync('/home/ubuntu/bitstarter/index.html', function (err, data) {
+    if (err) throw err;
+    var my_str = data.toString("ascii", 0, Buffer.byteLength(data,"ascii"));
+  });
+  response.send(my_str);
+/*
   response.send('Hello World 2!');
+*/
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
